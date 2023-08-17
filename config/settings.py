cb85a6e67593f795ecb196277a995ef9d20dba93
Doniyor Doniyorov <ddonierov96@gmail.com>
2023-08-17 08:04:27 +0500

    change static url

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = 'static-files/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
